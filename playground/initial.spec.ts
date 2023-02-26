eed7352109551c44f5ae2fb091b7e4a79a35304f
Peter Benko <peter.benko@xitaso.com>
2023-02-27 00:36:16 +0100

    Cleaned up example test

--- playground/initial.spec.ts
+++ playground/initial.spec.ts
@@ -2,9 +2,9 @@ import { cold, hot } from 'jest-marbles';
 import { mergeWith } from "rxjs";
 
 it('Should merge two hot observables and start emitting from the subscription point', () => {
-    const e1 = hot('----a--^--b   -------c--|', {});
-    const e2 = hot('  ---d-^--e   h--------f-----|', {});
-    const expected = cold('---(be)h------c-f-----|', {});
+    const e1 = hot('----a--^--b----------c--|');
+    const e2 = hot('  ---d-^--e---h--------f-----|');
+    const expected = cold('---(be)h------c-f-----|');
 
     expect(e1.pipe(mergeWith(e2))).toBeObservable(expected);
 });
